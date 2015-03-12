1f446bca941615036aac265c5bb022032ff17242
André de Jager <andrethehunter@gmail.com>
2015-03-12 22:05:00 +0000

    Added --no-files to theme completion
    See http://fishshell.com/docs/current/commands.html#complete

--- plugins/theme/completions/theme.fish
+++ plugins/theme/completions/theme.fish
@@ -1,4 +1,4 @@
 # Add completions when the theme is loaded. Thanks Bruno!
-complete --command (basename -s.fish (status -f)) \
+complete --no-files --command (basename -s.fish (status -f)) \
   --arguments "(basename (theme.util.get.themes))" \
   --description "Oh-my-fish theme"
